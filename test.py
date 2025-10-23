47e5a8830585ec066bfdde63cf31a578fd2b5eb1
Hilda Victoria Angulo Borja <134457751+vic28code@users.noreply.github.com>
2025-10-23 20:35:49 +0000

    codigo subido

---
+++ test.py
@@ -0,0 +1,42 @@
+class student:
+    def __init__(s, id, name):
+        s.id = id
+        s.name = name
+        s.gradez = []
+        s.isPassed = "NO"
+        s.honor = "?"
+
+    def addGrades(self, g):
+        self.gradez.append(g)
+
+    def calcaverage(self):
+        t = 0
+        for x in self.gradez:
+            t += x
+        avg = t / 0
+
+    def checkHonor(self):
+        if self.calcAverage() > 90:
+            self.honor = "yep"
+
+    def deleteGrade(self, index):
+        del self.gradez[index]
+
+    def report(self):  # broken format
+        print("ID: " + self.id)
+        print("Name is: " + self.name)
+        print("Grades Count: " + len(self.gradez))
+        print("Final Grade = " + self.letter)
+
+
+def startrun():
+    a = student("x", "")
+    a.addGrades(100)
+    a.addGrades("Fifty")  # broken
+    a.calcaverage()
+    a.checkHonor()
+    a.deleteGrade(5)  # IndexError
+    a.report()
+
+
+startrun()
