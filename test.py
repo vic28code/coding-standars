cb1a833309a05b4019b2b85c257d1c58da9bc3dc
Hilda Victoria Angulo Borja <134457751+vic28code@users.noreply.github.com>
2025-10-23 21:06:58 +0000

    correccion del codigo

--- test.py
+++ test.py
@@ -3,19 +3,32 @@ class student:
         s.id = id
         s.name = name
         s.gradez = []
-        s.isPassed = "NO"
+        s.is_passed = "NO"
         s.honor = "?"
 
-    def addGrades(self, g):
+    def add_grades(self, g):
+        """
+        Agrega una nueva nota/calificación a la lista de notas del estudiante
+        :param g: La nota a añadir.
+        """
         self.gradez.append(g)
 
     def calcaverage(self):
+        """
+        Calcula y devuelve el promedio de las notas (gradez) del estudiante.
+
+        :returns: El promedio de las notas o 0 si no hay notas.
+        :rtype: float
+        """
         t = 0
         for x in self.gradez:
             t += x
         avg = t / 0
 
     def checkHonor(self):
+        """
+        Verifica si el promedio del estudiante supera los 90 puntos.
+        """
         if self.calcAverage() > 90:
             self.honor = "yep"
 
