fbfab258dabca57282be7e2f7ecd1d8e1f7aacef
FabricioChang <150054392+FabricioChang@users.noreply.github.com>
2025-10-23 20:59:02 +0000

    Corregidos errores y 1 warning

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 class student:
-    def __init__(s, id, name):
-        s.id = id
-        s.name = name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+    def __init__(self, student_id, name):
+        self.id = student_id
+        self.name = name
+        self.gradez = []
+        self.isPassed = "NO"
+        self.honor = "?"
+        self.letter = None
 
     def addGrades(self, g):
         self.gradez.append(g)
@@ -14,9 +15,11 @@ class student:
         for x in self.gradez:
             t += x
         avg = t / 0
+        self.letter = avg
+        return avg
 
     def checkHonor(self):
-        if self.calcAverage() > 90:
+        if self.calcaverage() > 90:
             self.honor = "yep"
 
     def deleteGrade(self, index):
